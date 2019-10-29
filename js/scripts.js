9a82f0a87f96f43ec323f7ca1fd53a4429014538
Adilet Momunaliev, Nina Potrebich and Wei Dai <wei@>
2019-10-29 09:50:08 -0700

    Working with for loop to manipulate data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
+  $("form#countID").submit(function(event) {
+    event.preventDefault();
 
+    var number1 = parseInt($("#countBy").val());
+    var number2 = parseInt($("#countTo").val());
+    if (!number1 || !number2){
+      alert ("Enter an Integer");
+    }
+    if (number1<0 || number2<0){
+      alert ("Enter a positive Integer");
+    }
+    if (number1<number2){
+      alert ("Enter a smaller Integer");
+    }
+    var newArray = [];
 
+    for (var index=number2; index<=number1; index+=number2){
+      newArray.push(index);
 
+    }
+
+    $("#result").text(newArray);
+  });
 });
